3b6fb5f30eb60ead79326944323693d699f05633
beecodeguy <beecodeguy@gmail.com>
2025-02-02 13:06:49 +0545

    added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,43 @@
-import type { Metadata } from "next";
 import { Tomorrow } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./theme-provider";
 import Footer from "@/components/footer";
 import { Navbar } from "@/components/navbar";
+import { OG_IMAGE } from "@/consts";
 
 const ubuntu = Tomorrow({ subsets: ["latin"], weight: "400" });
 
-export const metadata: Metadata = {
-  title: "Gym Site",
+export const metadata = {
+  title: {
+    template: "%s | Gym Site",
+    default: "Gym Site",
+  },
   description: "Gym Site",
+  other: {
+    "color-scheme": "dark only",
+    "twitter:image": OG_IMAGE,
+    "twitter:card": "summary_large_image",
+    "og:url": "GYM",
+    "og:image": OG_IMAGE,
+    "og:type": "website",
+    robots: "index",
+  },
+  applicationName: "GYM",
+  creator: "BeecodeGuy",
+  publisher: "BeecodeGuy",
+  keywords: [
+    "GYM",
+    "FITNESS",
+    "HEALTH and FITNESS",
+    "FIT WORLD",
+    "EXERCISES",
+    "YOGA",
+  ],
+  formatDetection: {
+    email: true,
+    address: true,
+    telephone: true,
+  },
 };
 
 export default function RootLayout({
@@ -18,7 +46,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${ubuntu.className}`}>
         <ThemeProvider
           attribute="class"
--- consts.ts
+++ consts.ts
@@ -3,6 +3,8 @@ export const DEVELOPER_DETAILS = {
   developerUrl: "https://beecodeguy.github.io/",
 };
 
+export const OG_IMAGE = `https://images.unsplash.com/photo-1623874514711-0f321325f318?q=80&w=2070&auto=format&fit=crop`;
+
 export const NAV_LINKS = [
   {
     href: "/",
